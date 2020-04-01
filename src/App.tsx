ccc9a266748299a5ca6e8d2c0ca47fa81ed5922a
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-04-01 07:52:51 +0530

    started orm

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,7 @@
-import React, { useContext } from "react";
-import { useRequest } from "./hooks/useRequest";
-import { attr, ref } from "./hooks/payload";
-import { AppContext } from "./hooks/Provider";
+import React from "react";
 
 function App() {
-  const appContext: any = useContext(AppContext);
-
-  const { fetchData: fetchPosts, results: posts } = useRequest({
-    appContext,
-    url: "https://jsonplaceholder.typicode.com/posts",
-    payload: attr("array", attr("reference", { type: "many", ref: "posts" }))
-  });
-
-  const { fetchData: fetchPostById, results: postmain } = useRequest({
-    appContext,
-    url: "https://jsonplaceholder.typicode.com/posts/1",
-    payload: attr("object", attr("reference", { type: "one", ref: "posts" }))
-  });
-
-  const postmainref = ref(postmain, appContext);
-
-  return (
-    <div>
-      <button onClick={fetchPosts}>fetch posts</button>
-
-      <button onClick={fetchPostById}>fetch post by id</button>
-
-      <div>
-        <div
-          style={{
-            margin: 5,
-            padding: 5,
-            border: "1px solid green"
-          }}
-        >
-          <div key={postmainref?.id}>{postmainref?.title}</div>
-        </div>
-
-        <div
-          style={{
-            margin: 5,
-            padding: 5,
-            border: "1px solid green"
-          }}
-        >
-          {posts?.map((post: any) => {
-            const postref = ref(post, appContext);
-
-            return (
-              <div key={post.id}>
-                <div key={post.id}>{postref.title}</div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
+  return <div>hello</div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,13 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-import { models } from "./models";
-import { AppProvider } from "./hooks/Provider";
-import * as serviceWorker from "./serviceWorker";
+
+const App = lazy(() => import("./App"));
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppProvider models={models}>
+    <Suspense fallback={null}>
       <App />
-    </AppProvider>
+    </Suspense>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
